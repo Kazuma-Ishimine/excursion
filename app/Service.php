15c4b365f58016cf4d0087d31fc8c40bd35af231
Kazuma-Ishimine <r8a1wu2@gmail.com>
2022-01-06 19:57:21 +0900

    fix paginate()の後のget()の消去 サービス一覧が表示できるようにするため

--- app/Service.php
+++ app/Service.php
@@ -9,6 +9,6 @@ class Service extends Model
     public function getPaginateByLimitService(int $limit_count = 10)
     {
      // updated_atで降順に並べた後、limitで件数制限をかける
-     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count)->get();
+     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
