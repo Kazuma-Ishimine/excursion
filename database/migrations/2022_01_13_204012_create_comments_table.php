8dd60cc71ac418a900cfce6baa45cc11fdddd74a
Kazuma-Ishimine <r8a1wu2@gmail.com>
2022-01-13 20:48:37 +0900

    add commentsテーブルの作成 意見投稿ができるようにするため

---
+++ database/migrations/2022_01_13_204012_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            // id
+            $table->bigIncrements('id');
+            // Body(コメントの内容)
+            $table->string('body', 300);
+            // Review(いいねの数)
+            $table->Integer('review');
+            // created_atとupdated_at
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
