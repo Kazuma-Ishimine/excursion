3869527be185bbaf1334b904d6b1b413ee564407
Kazuma-Ishimine <r8a1wu2@gmail.com>
2022-01-14 14:44:00 +0900

    add 意見投稿作成画面を追加 意見を投稿することが出来るようにするため

---
+++ resources/views/comments/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>意見投稿一覧(仮)</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>仕事で遊ぶ</h1>
+        <!--入力フォーム-->
+        <form acrion='/comments' method='POST'>
+            <!--csrfトークンフィールド-->
+            @csrf
+            <!--内容入力-->
+            <div class='body'>
+                <h2>意見投稿内容</h2>
+                <!--内容入力用テキストエリア-->
+                <textarea name='post[body]' placeholder='意見投稿'></textarea>
+            </div>
+            <!--入力内容を送信するボタン-->
+            <input type='submit' value='投稿' />
+        </form>
+    </body>
+</html>
